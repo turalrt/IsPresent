34ef302e2198ae99a96f23f1e9618ada6a92b274
Tural Taghiyev <turalrt@code.edu.az>
2021-12-19 22:35:02 +0400

    first commit

---
+++ ConsoleApp33/Extensions/Extension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp33.Extensions
+{
+    public static class Extension
+    {
+        public static void IsPresent(this string str, string value)
+        {
+            bool result = str.Contains(value);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ConsoleApp33/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ConsoleApp33.Extensions;
+
+namespace ConsoleApp33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello World";
+            str.IsPresent("el");
+        }
+    }
+}
